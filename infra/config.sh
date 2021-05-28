af77495cd5d2780a296506f0d7b2f5bd8cdb65ea
Peter Marnef <peter.marnef@plantyn.com>
2021-05-28 13:54:22 +0200

    Add infra scripts

---
+++ infra/config.sh
@@ -0,0 +1,6 @@
+# Set shared environment variables
+
+LOCATION="westeurope"
+RESOURCE_GROUP="teammoodboard"
+APP_SERVICE_PLAN="teammoodboard"
+WEB_APP="teammoodboard"
---
+++ infra/create-azure-infra.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source config.sh
+
+echo "Creating Resource Group $RESOURCE_GROUP"
+az group create \
+    --name $RESOURCE_GROUP \
+    --location $LOCATION
+
+echo "Creating App Service Plan $APP_SERVICE_PLAN"
+az appservice plan create \
+    --resource-group $RESOURCE_GROUP \
+    --name $APP_SERVICE_PLAN \
+    --is-linux \
+    --sku S1
+
+echo "Creating Web App $WEB_APP"
+az webapp create \
+    --resource-group $RESOURCE_GROUP \
+    --plan $APP_SERVICE_PLAN \
+    --name $WEB_APP \
+    --runtime "DOTNET|5.0"
