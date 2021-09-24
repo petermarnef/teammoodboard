e1ccbf3677b330a3bac5efa4681fc8825bf32fd5
Peter Marnef <peter.marnef@plantyn.com>
2021-09-24 21:32:51 +0200

    Cleanup code with new .net 6 features

--- src/Client/Models/Client.cs
+++ src/Client/Models/Client.cs
@@ -2,13 +2,12 @@ using System;
 using MoodBoard.Shared;
 using Microsoft.AspNetCore.SignalR.Client;
 
-namespace MoodBoard.Models
+namespace MoodBoard.Models;
+
+public record Client
 {
-  public record Client
-  {
     public Guid SessionId { get; set; }
     public bool Initialised { get; set; }
     public Moodboard Moodboard { get; set; }
     public HubConnection HubConnection { get; set; }
-  }
 }
--- src/Shared/Moodboard.cs
+++ src/Shared/Moodboard.cs
@@ -1,38 +1,37 @@
 using System;
 using System.Collections.Generic;
 
-namespace MoodBoard.Shared
+namespace MoodBoard.Shared;
+
+public record Option(int Id, string Emoji); 
+public record Topic(int Id, string Name);
+public record Configuration(IList<Topic> Topics, IList<Option> Options);
+public record Vote(Guid ClientSessionId, int TopicId, int OptionId);
+
+public class Moodboard
 {
-    public record Option(int Id, string Emoji); 
-    public record Topic(int Id, string Name);
-    public record Configuration(IList<Topic> Topics, IList<Option> Options);
-    public record Vote(Guid ClientSessionId, int TopicId, int OptionId);
+    public Guid Id { get; init; }
+    public Configuration Configuration { get; init; }
+    public IList<Vote> Votes { get; init; }
 
-    public class Moodboard
+    public static Moodboard Initialise(Guid moodboardId)
     {
-      public Guid Id { get; init; }
-      public Configuration Configuration { get; init; }
-      public IList<Vote> Votes { get; init; }
-
-      public static Moodboard Initialise(Guid moodboardId)
-      {
-          return new Moodboard {
-            Id = moodboardId,
-            Configuration = new Configuration( 
-              new List<Topic> {
-                new Topic(1, "Team"),
-                new Topic(2, "Process"),
-                new Topic(3, "End Result"),
-                new Topic(4, "How I Felt")
-            },
-              new List<Option> {
-                new Option(1, "🙂"),
-                new Option(2, "😐"),
-                new Option(3, "😥")
-                // new Option(4, "🤪")
-            }),
-            Votes = new List<Vote>()
-          };
-      }
+        return new Moodboard {
+        Id = moodboardId,
+        Configuration = new Configuration( 
+            new List<Topic> {
+            new Topic(1, "Team"),
+            new Topic(2, "Process"),
+            new Topic(3, "End Result"),
+            new Topic(4, "How I Felt")
+        },
+            new List<Option> {
+            new Option(1, "🙂"),
+            new Option(2, "😐"),
+            new Option(3, "😥")
+            // new Option(4, "🤪")
+        }),
+        Votes = new List<Vote>()
+        };
     }
 }
